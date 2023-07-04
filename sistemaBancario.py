f4cbc6ade4159d4b9f51988fea3a24de8481fe18
Victor Pereira <82619763+Dnx0@users.noreply.github.com>
2023-07-04 12:29:48 -0300

    Sistema de caixa com continue
    Agora o saldo está impossível de ficar negativo, e tem o continue ao invés do break

--- sistemaBancario.py
+++ sistemaBancario.py
@@ -15,24 +15,28 @@ while opcao != 0:
     opcao = int(input("[1] Para sacar \n [2] Para depositar \n [3] Para extrato \n [0] Para sair \n :"))
     #Opção de Saque
     if opcao == 1 :
-      saque = float(input("Digite o valor a ser sacado"))                     
+      saque = float(input("Digite o valor a ser sacado: "))                     
       if saldo <= 0 :
         print ("Conta zerado, não é possivel realizar o saque")
+        continue
       if saldo < saque:
          print("Não há fundos suficientes para realizar o saque!!!") 
+         continue
       if saque > 500:
          print ("Valor indisponivel para saque, somente saques abaixo de 500")
+         continue
       if saque <= 0:
          print("Erro, valor impossivel de sacar")
       if   saqueDia <= 0:
          print("Sem saques diarios restantes") 
+         continue
       else:
          saqueDia -= 1
          saldo -= saque
          print("Valor sacado com sucesso.")
     #Opção de Deposito
     elif opcao == 2 :
-        deposito = float(input("Digite o valor do deposito:"))
+        deposito = float(input("Digite o valor do deposito: "))
         if deposito > 0:
            saldo += deposito
            print("Deposito realizado com sucesso.")
