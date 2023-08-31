93e5693385461cba4648e93e512865136a457e59
Victor <victor.pereira@tutanota.com>
2023-08-31 16:44:09 -0300

    Sistema bancario 2.0

--- desafioSobremesa.py
+++ desafioSobremesa.py
@@ -1,11 +1,14 @@
-valorPedido = int(input("Qual foi o valor do pedido: "))
+valorPedido = float(input("Qual foi o valor do pedido: "))
 i = 0
+sobremesa = 50
 
 while i == 0:
-  if valorPedido >= 50:
+  if valorPedido >= sobremesa:
     print("Parabens, você ganhou uma sobremesa gratis!")
     i += 1
   else:
     print("Que pena, você nao ganhou nenhum brinde especial.")
+    print(f"Faltou { sobremesa - valorPedido } reais para ganhar o brinde")
     i += 1
 
+  
